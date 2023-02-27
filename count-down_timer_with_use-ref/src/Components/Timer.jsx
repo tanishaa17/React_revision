3e8d0714fa35258d8a335318546122e4d10f3269
tanishaa17 <krati1701@gmail.com>
2023-02-27 22:57:47 +0530

    error fixed

--- count-down_timer_with_use-ref/src/Components/Timer.jsx
+++ count-down_timer_with_use-ref/src/Components/Timer.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import styles from './Timer.module.css';
 
 export default function Timer() {
-    const [count, SetCount] = useState(10);
+    const [count, setCount] = useState(10);
+    let intervalId = useRef(null);
+    // useEffect(() => { }, []);
     const handleStart = () => {
-        console.log(`Button Clicked`);
+        // console.log(`Button Clicked`);
+        intervalId = setInterval(() => {
+            setCount((newCount) => {
+                if (newCount <= 1) {
+                    clearInterval(intervalId);
+                    return 0;
+                }
+                console.log(`Timer is running fine`)
+                return newCount - 1;
+            });
+        }, 1000);
     }
     const handleStop = () => {
-        console.log(`Button Clicked`);
+        // console.log(`Button Clicked`);
+        // if (newCount <= 1) {
+        clearInterval(intervalId);
+        // return 0;
+        // }
     }
     const handleReset = () => {
-        console.log(`Button Clicked`);
+        // console.log(`Button Clicked`);
     }
 
     return (
         <>
             <h2>Timer : {count}</h2>
-
             <button className={styles.btn} onClick={handleStart}>Start</button>
             <button className={styles.btn} onClick={handleStop}>Stop</button>
             <button className={styles.btn} onClick={handleReset}>Reset</button>
