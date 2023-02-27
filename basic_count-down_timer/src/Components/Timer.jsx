2e931daf5bbcce7742dfc97a8c1c98c0b654f036
tanishaa17 <krati1701@gmail.com>
2023-02-27 13:32:26 +0530

    all done

--- basic_count-down_timer/src/Components/Timer.jsx
+++ basic_count-down_timer/src/Components/Timer.jsx
@@ -1,8 +1,26 @@
 import { useEffect, useState } from "react";
 
 const Timer = () => {
-  const [count, setCount] = useState(15);
-
+  const [count, setCount] = useState(10);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // console.log({ count });
+      // console.log(count, "prev"); It would be taking iniatial value only i.e. outdated value
+      // console.log("timer running");
+      setCount((newCount) => {
+        if (newCount <= 1) {
+          clearInterval(intervalId);
+          return 0;
+        }
+        return newCount - 1;
+      });
+    }, 1000);
+    const cleanUpFunc = () => {
+      clearInterval(intervalId);
+      // console.log("Cleanup called");
+    };
+    return cleanUpFunc;
+  }, []);
 
   return (
     <>
