5ce8c4e05bbd3dc6d55d65628f115c0263bbc84b
tanishaa17 <krati1701@gmail.com>
2023-03-01 14:28:18 +0530

    rendered the users

--- fake_chat_app/src/Components/ChatApp.jsx
+++ fake_chat_app/src/Components/ChatApp.jsx
@@ -1,13 +1,34 @@
-
+import { useState } from "react";
 import createConnection, { users } from "../Utils/chat";
 import Contacts from "./Contacts";
 
 function ChatApp() {
+    const [messages, setMessages] = useState([]);
+    const [subscribedTo, setSubscribedTo] = useState(users[0]);
+
+    // - const {listen, unsubscribe} = createConnection( currentUser )
+    // - const connection = createConnection( currentUser )
+    // - listen( callback )
+    // - the callback will be given any new message
+    // - so you can write listen( message => {...your code} )
+    // - you need to ensure you are unsubscribing from user1 to user2 as well
+    // - display all the messages on the UI
+    // - when changing user, messages should be reset
+
 
 
     return (
         <div>
             <h1>Contacts</h1>
+            <Contacts
+                // all users
+                users={users}
+                // active user
+                active={subscribedTo}
+                // onChange
+                onChange={(user) => setSubscribedTo(user)}
+            />
+            <hr />
 
         </div>
     );
