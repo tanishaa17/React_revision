4e8b0cf99087d0f06821206164fadd6ca5d6286d
tanishaa17 <krati1701@gmail.com>
2023-03-01 17:01:55 +0530

    added a file and with random text

--- context-api-revision/src/App.jsx
+++ context-api-revision/src/App.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from './Components/Navbar';
 import './App.css';
+import { Main } from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ context-api-revision/src/Components/Main.jsx
@@ -0,0 +1,19 @@
+export const Main = () => {
+    return (
+        <>
+            <h2>Fun</h2>
+            Seriously, this is a fun generator to just enjoy with no other purpose in mind. There are a lot of people who just happen across this free tool, but once they find it, spend much more time than they anticipated generating unusual random words again and again. Time may fly by as you play with this tool.
+
+            <h2> New Word of the Day</h2>
+            If you're the type of person who loves to learn a new word a day, this may be the perfect tool for you to visit each morning. Since the words in the generator are fun and quirky, it's actually a lot of fun (and not a chore) learning that new word a day. In fact, they're the type of words that you're likely to share with your friends or try to use in a conversation just to see the surprised looks on all the other people's faces when you use it.
+
+            <h2>Expand Your Vocabulary</h2>
+            Beyond learning a new word a day, this tool is an excellent way to expand your vocabulary in an easy, interesting way. While going through vocabulary lists can take a lot of time and at times be quite boring, you're guaranteed to have some unusual word pop up with every click which can make it a lot of fun. Improving your vocabulary doesn't need to be a drag.
+
+            <h2> Guessing Game</h2>
+            This generator can also provide a lot of fun when you're with friends. All you need to do is generate a random unusual word and then let your friends try and guess the meaning of it. Again, this is a great way to group learn new vocabulary in a nontraditional study method. The guesses of what the words mean can often be as entertaining as the unusual words themselves.
+
+            Above are just a few ways this tool can be used. If you happen to use this generator on a fairly regular basis or are using it for a specific project, we would appreciate you taking a moment to let us know exactly how you're using it. While we usually have a general idea of how the tools we create will be used, we're often surprised at some of the creative ways they end up being used we never anticipated. By understanding the various ways it's being used gives us the opportunity to make improvements to it as we update, ultimately making it better for everyone. In addition, if you have a specific suggestion on how we could improve the Random Unusual Word Generator, we'd love to get your ideas.
+        </>
+    )
+}
