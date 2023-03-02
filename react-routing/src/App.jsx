a167c63b2d4560fccb807fb8a10692fc4b77272d
tanishaa17 <krati1701@gmail.com>
2023-03-02 14:42:43 +0530

    Basic structure created

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { About } from './Routes/About';
+import { Contacts } from './Routes/Contacts';
+import { Home } from './Routes/Home';
+import { Login } from './Routes/Login';
+import { Users } from './Routes/Users';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/about' element={<Contacts />} />
+        <Route path='/about' element={<Login />} />
+        <Route path='/about' element={<Users />} />
+      </Routes>
     </div>
   );
 }
