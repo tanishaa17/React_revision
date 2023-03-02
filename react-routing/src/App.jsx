29cf9a197bbcf6c10075d967880fc2d0e21d6dd9
tanishaa17 <krati1701@gmail.com>
2023-03-02 14:48:36 +0530

    Links created

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { About } from './Routes/About';
 import { Contacts } from './Routes/Contacts';
@@ -9,12 +9,18 @@ import { Users } from './Routes/Users';
 function App() {
   return (
     <div className="App">
+
+      <Link to="/about">About</Link>
+      <Link to="/contacts">Contacts</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/user">Users</Link>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/about' element={<Contacts />} />
-        <Route path='/about' element={<Login />} />
-        <Route path='/about' element={<Users />} />
+        <Route path='/contacts' element={<Contacts />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/user' element={<Users />} />
       </Routes>
     </div>
   );
