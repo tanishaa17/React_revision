f01ce37e4dd3b0086f61504d8096174e341ccb25
tanishaa17 <krati1701@gmail.com>
2023-03-02 20:13:25 +0530

    rendered the data

--- react-routing/src/Components/Navbar.jsx
+++ react-routing/src/Components/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from "./Navbar.module.css"
+const links = [
+    { to: "/", title: "Home" },
+    { to: "/about", title: "About" },
+    { to: "/contacts", title: "Contacts" },
+    { to: "/login", title: "Login" },
+    { to: "/user", title: "Users" }
+]
 export const Navbar = () => {
-    const links = [
-        { to: "/", title: "Home" },
-        { to: "/about", title: "About" },
-        { to: "/contacts", title: "Contacts" },
-        { to: "/login", title: "Login" },
-        { to: "/user", title: "Users" }
-    ]
+    const activeLink = { color: "black" };
+    const defaultLink = { color: "rgb(20, 80, 141)" };
     return (
         <div className={styles.nav}>
-            {links.map((elem) => (
-                <Link className={styles.link} to={elem.to}>{elem.title}</Link >
+            {links.map(({ to, title }) => (
+                <NavLink
+                    style={({ isActive }) => {
+                        return isActive ? activeLink : defaultLink;
+                    }}
+                    key={to} className={styles.link} to={to}>{title}</NavLink >
             ))}
             {/* <Link className={styles.link} to="/">Home</Link>
             <Link className={styles.link} to="/about">About</Link>
-            <Link className={styles.link} to="/contacts">Contacts</Link>
-            <Link className={styles.link} to="/login">Login</Link>
             <Link className={styles.link} to="/user">Users</Link> */}
 
         </div>
--- react-routing/src/Pages/Users.jsx
+++ react-routing/src/Pages/Users.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import styles from "./Users.module.css"
 export const Users = () => {
-    return (
-        <div>Users</div>
-    )
+    const [userData, setUserData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [err, setErr] = useState(false);
+
+
+    const fetchData = async (url) => {
+        try {
+            let res = await fetch(url);
+            let data = await res.json();
+            // console.log(data.data);
+            setLoading(true);
+            setUserData(data.data);
+            setLoading(false);
+        } catch (error) {
+            setErr(true);
+            setLoading(false)
+            // console.log("Something went wrong");
+        }
+    }
+    useEffect(() => {
+        fetchData(`https://reqres.in/api/users`);
+        // console.log(userData);
+    }, [])
+
+    return loading ? <h1>Loading.....</h1> :
+        err ? <h1>"Something went wrong"</h1> :
+            (
+                <>
+                    <h2 >User data</h2>
+                    {userData.map(({ avatar, first_name, last_name }) => {
+                        return (
+                            <div className={styles.user}>
+                                <div> <img src={avatar} alt="img" /></div>
+                                <span >{first_name + " " + last_name}</span> <hr />
+                            </div>
+                        )
+                    })}
+                </>
+            )
 }
