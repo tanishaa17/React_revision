68e895978ee245a2c7de983c4eea8467b96c8ec3
tanishaa17 <krati1701@gmail.com>
2023-03-02 16:01:30 +0530

    Made it dynamic

--- react-routing/src/Components/Navbar.jsx
+++ react-routing/src/Components/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import styles from "./Navbar.module.css"
 export const Navbar = () => {
+    const links = [
+        { to: "/", title: "Home" },
+        { to: "/about", title: "About" },
+        { to: "/contacts", title: "Contacts" },
+        { to: "/login", title: "Login" },
+        { to: "/user", title: "Users" }
+    ]
     return (
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around", fontWeight: "bolder", fontSize: "30px" }}>
-
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contacts">Contacts</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/user">Users</Link>
+        <div className={styles.nav}>
+            {links.map((elem) => (
+                <Link className={styles.link} to={elem.to}>{elem.title}</Link >
+            ))}
+            {/* <Link className={styles.link} to="/">Home</Link>
+            <Link className={styles.link} to="/about">About</Link>
+            <Link className={styles.link} to="/contacts">Contacts</Link>
+            <Link className={styles.link} to="/login">Login</Link>
+            <Link className={styles.link} to="/user">Users</Link> */}
 
         </div>
     )
