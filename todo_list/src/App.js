2ce941eb8317dced0efc708c6cf8ae86c66652b3
tanishaa17 <krati1701@gmail.com>
2023-03-05 23:40:55 +0530

    .

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import TodoList from "./Components/TodoList"
 export default function App() {
   return (
     <div className="App">
       <h1>To-Do List</h1>
-
+      <TodoList />
     </div>
   );
 }
--- todo_list/src/Components/TodoList.jsx
+++ todo_list/src/Components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const TodoList = () => {
+export default function TodoList() {
     return (
         <div>TodoList</div>
     )
