4f073bfa01e2c062cf8dcef1b2479a69213a7d4a
tanishaa17 <krati1701@gmail.com>
2023-03-05 23:36:00 +0530

    folder structure created

---
+++ todo_list/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+
+    </div>
+  );
+}
---
+++ todo_list/src/Components/AddItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddItem = () => {
+    return (
+        <div>AddItem</div>
+    )
+}
