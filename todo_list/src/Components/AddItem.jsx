c5f0ce327f57c2f8aa2d7cc5b4389ea1efd86c80
tanishaa17 <krati1701@gmail.com>
2023-03-06 23:27:40 +0530

    onchange and add button added

--- todo_list/src/Components/AddItem.jsx
+++ todo_list/src/Components/AddItem.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useState } from "react";
+const AddItem = ({ handleAdd }) => {
+    const [text, setText] = useState("");
 
-export const AddItem = () => {
+    // to set the value in input box
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+    const handleClick = () => {
+        handleAdd(text);
+        setText("");
+    };
     return (
-        <div>AddItem</div>
-    )
-}
+        <>
+            <input
+                onChange={handleChange}
+                value={text}
+                type="text"
+                placeholder="Add new item"
+            />
+            <button disabled={text === ""} onClick={handleClick}>
+                Add
+            </button>
+        </>
+    );
+};
+export default AddItem;
