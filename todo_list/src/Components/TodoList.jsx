37d3fc82d983cdac9f24a88bbee0a42c9ac6bae7
tanishaa17 <krati1701@gmail.com>
2023-03-07 23:25:25 +0530

    .

--- todo_list/src/Components/TodoList.jsx
+++ todo_list/src/Components/TodoList.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+import AddItem from "./AddItem";
+import TodoItems from "./TodoItems";
 
-export default function TodoList() {
+const TodoList = () => {
+    const [todo, setTodo] = useState([]);
+    const handleAdd = (text) => {
+        const addNewItem = {
+            title: text,
+            status: false,
+            id: Date.now() + Math.random()
+        };
+        // console.log(addNewItem.id);
+        const newTodo = [...todo, addNewItem];
+        // console.log(newTodo);
+        setTodo(newTodo);
+    };
     return (
-        <div>TodoList</div>
-    )
-}
+        <>
+            <AddItem handleAdd={handleAdd} />
+            {/* <TodoItems /> */}
+            <div>
+                {todo.map((el) => (
+                    <TodoItems key={el.id} el={el} />
+                ))}
+            </div>
+        </>
+    );
+};
+export default TodoList;
