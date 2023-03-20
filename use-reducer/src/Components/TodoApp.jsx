9d5178b292f63cede84350c34674556f8c01b7fc
tanishaa17 <krati1701@gmail.com>
2023-03-20 23:56:06 +0530

    dispatcher function  added

--- use-reducer/src/Components/TodoApp.jsx
+++ use-reducer/src/Components/TodoApp.jsx
@@ -1,26 +1,49 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import { TodoInput } from './TodoInput'
 
-export const TodoApp = () => {
-    const [todos, setTodos] = useState([])
-    const handleAdd = ((value) => {
-        setTodos([...todos, { id: Date.now(), value, isCompleted: false }])
-    })
-    const toggleStatus = ((id) => {
-        setTodos(
-            todos.map((todo) => {
-                if (todo.id === id) todo.isCompleted = !todo.isCompleted;
-                return todo
+
+const todoReducerFunc = ((state, { type, payload }) => {
+    switch (type) {
+        case "ADD": {
+            state = [...state, { id: Date.now(), value: payload, isCompleted: false }]
+            return state
+        }
+        case "TOGGLE": {
+            const newState = state.map((todo) => {
+                if (todo.id === payload) todo.isCompconsted = !todo.isCompleted;
+                return { ...todo }
             })
-        )
-    })
-    const handleDelete = ((id) => {
-        setTodos(todos.filter((el) => el.id !== id))
-    })
+            return newState
+        }
+        case "DELETE": {
+            let newState = state.filter((el) => el.id !== payload)
+            return newState
+        }
+        default: {
+            return state
+        }
+    }
+})
+export const TodoApp = () => {
+    const [todos, dispatch] = useReducer(todoReducerFunc, [])
+    // const handleAdd = ((value) => {
+    //     setTodos([...todos, { id: Date.now(), value, isCompleted: false }])
+    // })
+    // const toggleStatus = ((id) => {
+    //     setTodos(
+    //         todos.map((todo) => {
+    //             if (todo.id === id) todo.isCompleted = !todo.isCompleted;
+    //             return todo
+    //         })
+    //     )
+    // })
+    // const handleDelete = ((id) => {
+    //     setTodos(todos.filter((el) => el.id !== id))
+    // })
     return (
         <div>
             <h1>Todo App</h1>
-            <TodoInput handleAdd={handleAdd} />
+            <TodoInput dispatch={handleAdd} />
 
             {todos.map((todo) => (
                 <div key={todo.id}>
