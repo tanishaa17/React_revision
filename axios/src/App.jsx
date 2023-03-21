877f498fd03ea325753f294a7c8f9e146e2b9acd
tanishaa17 <krati1701@gmail.com>
2023-03-21 19:46:56 +0530

    posted data with axios

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -5,14 +5,20 @@ import axios from 'axios';
 
 export const App = () => {
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/users`,
-    {
-      params:{
-        id:2
-      }
-    })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+    // axios.get(`https://jsonplaceholder.typicode.com/users`,
+    // {
+    //   params:{
+    //     id:2
+    //   }
+    // })
+    //   .then((res) => console.log(res))
+    //   .catch((err) => console.log(err));
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, {
+      userId: 5,
+      title: "anything",
+      body: "Something about anything"
+    }).then((res) => console.log(res))
   }, [])
   return (
     <div>App</div>
